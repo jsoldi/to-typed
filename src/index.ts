40b3f639de5aab950adf70864213a94613fa3d96
Juan Soldi <jsoldi@hotmail.com>
2022-06-21 19:22:15 +0200

    Commented out junk

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,32 @@
 export { Maybe, Cast, Guard, Convert } from "./internal.js";
 
-import { Cast, Convert, Guard, Maybe, Utils } from "./internal.js";
+// import { Cast, Convert, Guard, Maybe, Utils } from "./internal.js";
 
-function tela<T>(value: T) {
-    return Cast.just(value).asString.bind(str1 => Cast.asString.if(str2 => {
-        console.log(`str1: ${str1}, str2: ${str2}`);
-        return str1 === str2;
-    })).map(_ => value);
-}
+// function tela<T>(value: T) {
+//     return Cast.just(value).asString.bind(str1 => Cast.asString.if(str2 => {
+//         console.log(`str1: ${str1}, str2: ${str2}`);
+//         return str1 === str2;
+//     })).map(_ => value);
+// }
 
-function testea(value: unknown) {
-    const test = Convert.toArray().map(ar => ar[0]);
+// function testea(value: unknown) {
+//     const test = Convert.toArray().map(ar => ar[0]);
 
-    const may = test.cast(value);
+//     const may = test.cast(value);
 
-    if (may.hasValue) {
-        console.log('has value');
-        return may.elseThrow();
-    }
-    else {
-        console.log('has no value');
-        return 'NOPOPO';
-    }
-}
+//     if (may.hasValue) {
+//         console.log('has value');
+//         return may.elseThrow();
+//     }
+//     else {
+//         console.log('has no value');
+//         return 'NOPOPO';
+//     }
+// }
 
-function test() {
-    const palas = testea(123);
-    debugger;
-}
+// function test() {
+//     const palas = testea(123);
+//     debugger;
+// }
 
-test();
+// test();
