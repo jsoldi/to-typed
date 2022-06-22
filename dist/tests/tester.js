84922e53be1d7719a46c0a4b7c31f3e2bab58a99
Juan Soldi <jsoldi@hotmail.com>
2022-06-22 10:04:43 +0200

    Added tests

--- dist/tests/tester.js
+++ dist/tests/tester.js
@@ -1,9 +1,25 @@
+const testInfo = {
+    started: false,
+    passed: 0,
+    failed: 0,
+    tests: new Set()
+};
 export function test(name, action) {
+    if (!testInfo.started) {
+        testInfo.started = true;
+        setTimeout(() => console.log(`✔️  ${testInfo.passed} passed, ${testInfo.failed} failed`), 1);
+    }
+    if (!testInfo.tests.has(name))
+        testInfo.tests.add(name);
+    else
+        throw new Error(`Test ${name} already exists`);
     try {
         action();
+        testInfo.passed++;
         console.log(`✅ ${name}: passed`);
     }
     catch (e) {
+        testInfo.failed++;
         console.error(`❌ ${name}: ${e}`);
     }
 }
--- src/tests/tester.ts
+++ src/tests/tester.ts
@@ -1,9 +1,28 @@
+const testInfo = {
+    started: false,
+    passed: 0,
+    failed: 0,
+    tests: new Set<string>()
+}
+
 export function test(name: string, action: () => void) {
+    if (!testInfo.started) {
+        testInfo.started = true
+        setTimeout(() => console.log(`✔️  ${testInfo.passed} passed, ${testInfo.failed} failed`), 1);
+    }
+
+    if(!testInfo.tests.has(name))
+        testInfo.tests.add(name)
+    else
+        throw new Error(`Test ${name} already exists`)
+
     try {
         action();
+        testInfo.passed++;
         console.log(`✅ ${name}: passed`);
     }
     catch (e: any) {
+        testInfo.failed++;
         console.error(`❌ ${name}: ${e}`);
     }
 }
